2d50037dc673391538f3c02654d032d2df2e509c
strgPeter <peter2003lei@gmail.com>
2025-03-30 12:33:30 +0200

    Implemented mail client

---
+++ src/main/java/org/example/MailClient.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+import java.util.Properties;
+import java.util.Set;
+
+public class MailClient {
+
+    private static final String username = System.getenv("MAIL_USERNAME");
+    private static final String password = System.getenv("MAIL_PASSWORD");
+
+    public static void send(String subject, String text, Set<String> recipients) throws MessagingException {
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "465");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.socketFactory.port", "465");
+        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+        Session session = Session.getInstance(prop,
+                new jakarta.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(username));
+        message.setRecipients(
+                Message.RecipientType.TO,
+                InternetAddress.parse(String.join(", ", recipients))
+        );
+        message.setSubject(subject);
+        message.setText(text);
+
+        Transport.send(message);
+    }
+
+    public static void send(String subject, String text, String recipient) throws MessagingException {
+        send(subject, text, Set.of(recipient));
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
 package org.example;
 
+import jakarta.mail.MessagingException;
+
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -11,16 +15,53 @@ public class Main {
 
         final int FUEL_THRESHOLD = 600;
         final int CO2_THRESHOLD = 120;
+        final Set<String> MAIL_RECIPIENTS = Set.of(System.getenv("MAIL_RECIPIENT_1"));
+        final String MAIL_ADMIN = System.getenv("MAIL_RECIPIENT_1");
 
         ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
         ses.scheduleAtFixedRate(() -> {
             Optional<ScrapeResult> optionalScraperResult = Scraper.getData();
 
-            if (optionalScraperResult.isEmpty()) return;
+            if (optionalScraperResult.isEmpty()) {
+                try {
+                    MailClient.send(
+                            "Something wend wrong retrieving data - Empty optional",
+                            "Admin Mail",
+                            MAIL_ADMIN
+                    );
+                } catch (MessagingException e) {
+                    System.out.println("Could not send mail to admin!");
+                }
+                return;
+            }
 
             ScrapeResult scraperResult = optionalScraperResult.get();
 
+            int f = scraperResult.getFuel();
+            int c = scraperResult.getCo2();
 
+            String msg = """
+                    Fuel price = %d
+                    Co2 price = %d
+                    This message was sent by an automated mail bot by Peter.
+                    (https://github.com/strgPeter)
+                    """.formatted(f, c);
+
+            try {
+                if (f <= FUEL_THRESHOLD && c > CO2_THRESHOLD) {
+                    MailClient.send(String.format("Fuel: %d", f), msg, MAIL_RECIPIENTS);
+                    System.out.println("Sent fuel notification");
+                } else if (c <= CO2_THRESHOLD && f > FUEL_THRESHOLD) {
+                    MailClient.send(String.format("Co2: %d", c), msg, MAIL_RECIPIENTS);
+                    System.out.println("Sent co2 notification");
+                } else if (c <= CO2_THRESHOLD && f <= FUEL_THRESHOLD) {
+                    MailClient.send(String.format("Fuel: %d; Co2: %d", f, c), msg, MAIL_RECIPIENTS);
+                    System.out.println("Sent fuel and co2 notification");
+                }
+            } catch (MessagingException e) {
+                System.out.println("An error occurred while sending mail notification");
+                System.out.println(e.getMessage());
+            }
 
         }, 0, 30, TimeUnit.MINUTES);
 
